286394ca4f1b8b00a77cd2e380dae9827450df47
Bogdan Rizac <bogdan-rizac@users.noreply.github.com>
2022-05-16 09:51:31 +0200

    update outputs

--- main.tf
+++ main.tf
@@ -2,12 +2,16 @@ output "name" {
   value = "github-atlantis-hook"
 }
 
-output "data" {
-  value = "my new data"
+output "data-1" {
+  value = "my data 1"
 }
 
 output "data-2" {
-  value = "my new data"
+  value = "my data 2"
+}
+
+output "data-3" {
+  value = "my data 3"
 }
 
 #
