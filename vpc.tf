07e1b495db21df2a035e911ac8d639eaf8862227
Bogdan Rizac <bogdan-rizac@users.noreply.github.com>
2022-06-28 14:12:30 +0200

    show more output from tfsec

--- vpc.tf
+++ vpc.tf
@@ -1,28 +1,13 @@
 
-locals {
-  params = yamldecode(file("../parameters.yaml"))
+resource "aws_s3_bucket" "demo" {
+  bucket = "demo-123"
 }
 
-module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
-  version = "v3.13.0"
+resource "aws_s3_bucket" "demo2" {
+  bucket = "demo-1234"
+}
 
-  name = "devops-acg"
-  cidr = "10.0.0.0/16"
-
-  azs = ["us-east-1a", "us-east-1b", "us-east-1c"]
-
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-
-  enable_nat_gateway = true
-  single_nat_gateway = true
-  #  enable_vpn_gateway = false
-
-  enable_dns_hostnames = true
-  enable_dns_support = true
-
-  tags = {
-    Generator = "Terraform"
-  }
+resource "aws_instance" "demo" {
+  instance_type = "t3.small"
+  ami = "ami-1231231232"
 }
